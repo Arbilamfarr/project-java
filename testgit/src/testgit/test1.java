41c1a97a4838a424a2fa22a1c4581a80efd50c4e
elarbi <arbilamfarrad@gmail.com>
2024-01-21 20:19:35 +0100

    modified

--- testgit/src/testgit/test1.java
+++ testgit/src/testgit/test1.java
@@ -5,6 +5,9 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("bonjour tous le monde ");
+   System.out.println("bonjour tous le monde ");
+   System.out.println("bonjour tous le monde ");
+   System.out.println("bonjour tous le monde ");
 	}
 
 }
